350c9193b4045bcc802a3400e2a8fd46ad1f3e5b
blubbundbla <blubbundbla@yahoo.com>
2020-03-09 11:33:29 +0100

    update email

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='ncbiTAXONparser',
       version='0.1',
       description='parser for ncbi taxonomy information',
       author='Martha Kandziora',
-      author_email='martha.kandziora@yahoo.com',
+      author_email='martha.kandziora@mailbox.org',
       packages=['ncbiTAXONparser'],
-     )
+      )
